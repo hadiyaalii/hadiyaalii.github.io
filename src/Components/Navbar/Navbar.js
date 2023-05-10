fd3d7612c88f22312263ff791bef4d1ab12d619b
quddossaoudi <quddossaoudi@gmail.com>
2023-05-11 03:06:08 +0800

    bug fixes and tab display improved

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -143,7 +143,7 @@ function Navbar() {
                   </Text>
                 </Link>
               </span>
-              <Link to="/equipments">
+              <Link to="https://hadiyaalii.github.io//equipments">
                 <Text
                   color="Gray"
                   _hover={
